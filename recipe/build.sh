3ec3fe297dfcbd5fe6d23cbe2f954daacf4104de
Marco Meyer-Conde <marco@tcu.ac.jp>
2026-07-07 13:20:08 +0900

    Fill in real source, license, and build steps for CutTools 2.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/bash
 set -e
 
-mkdir -p build
-cd build
+cd source
 
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_BUILD_TYPE=Release
+# Enable extended-source with '-ffixed-line-length-none' for long lines
+make PRECISION=QP FC="${FC}" FFLAGS="${FFLAGS} -std=legacy -fno-automatic -O2 -funroll-all-loops -ffixed-line-length-none -I${PREFIX}/include/mpfun90 -I${PREFIX}/include/oneloop"
 
-NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
-make -j"$NPROC"
-make install
+# Avoid vendoring FF headers
+rm -f includects/ffs.h includects/ff.h includects/aa.h
+
+mv includects/libcts.a "${PREFIX}/lib/"
+mkdir -p "${PREFIX}/include/cts"
+mv includects/*.h "${PREFIX}/include/cts/"
+mv includects/*.mod "${PREFIX}/include/cts/"
+
+make clean
